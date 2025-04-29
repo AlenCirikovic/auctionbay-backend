44b97605d60c2f65a5af55d8c4efb6605e7c21b6
AlenCirikovic <alen.ciriko@gmail.com>
2025-04-29 23:21:06 +0200

    Added CRUD requests to Users controller.

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma, User } from 'generated/prisma';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() createUserDto: Prisma.UserCreateInput){
+        return this.usersService.create(createUserDto)
+    }
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    findOne(@Param('id') id: string){
+        return this.usersService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string, @Body() updateProductDto: Prisma.UserUpdateInput){
+        return this.usersService.update(id,updateProductDto)
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return this.usersService.remove(id)
+    }
+
+}
